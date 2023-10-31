142106fba3a5a9ca6f3895219491b142116840ae
Martin <MartinHovhannisyan31@gmail.com>
2023-10-31 23:48:34 +0400

    recursion_1

--- index.ts
+++ index.ts
@@ -1,56 +1,19 @@
-abstract class Human {
-  name: string;
-  constructor(name: string) {
-      this.name = name;
+//узнаем число подмассивов
+function arraysCount(arr: any[]): number {
+  let count = 0;
+  let index = 0;
+
+  while (index < arr.length) {
+    if (Array.isArray(arr[index])) {
+      count++; 
+      count += arraysCount(arr[index]);
+    }
+    index++;
   }
-  abstract work(): void;
+
+  return count;
 }
 
-class Programmer extends Human {
-  work() {
-      console.log(`${this.name} is coding.`);
-  }
-}
-
-class ProjectManager extends Human {
-  project: Programmer[];
-  constructor(name: string, project: Programmer[]) {
-      super(name);
-      this.project = project;
-  }
-  work() {
-      console.log(`${this.name} is managing the project.`);
-      this.project.forEach((programmer) => programmer.work());
-  }
-}
-
-class CEO extends Human {
-  projects: ProjectManager[];
-  constructor(name: string, projects: ProjectManager[]) {
-      super(name);
-      this.projects = projects;
-  }
-  work() {
-      console.log(`${this.name} is overseeing the company.`);
-      this.projects.forEach((project) => project.work());
-  }
-}
-
-class Company {
-  ceo: CEO;
-  constructor(ceo: CEO) {
-      this.ceo = ceo;
-  }
-  startWork() {
-      this.ceo.work();
-  }
-}
-
-
-let programmer1 = new Programmer("Programmer 1");
-let programmer2 = new Programmer("Programmer 2");
-let projectManager = new ProjectManager("Project Manager", [programmer1, programmer2]);
-let ceo = new CEO("CEO", [projectManager]);
-let company = new Company(ceo);
-
-company.startWork();
+const arr: any[] = [1, 2, 3, [1, 2], [1, [1, 2]]];
+const result: number = arraysCount(arr);
+console.log(`qanak: ${result}`);
